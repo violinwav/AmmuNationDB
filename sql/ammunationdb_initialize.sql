00318e69920a3815773418eab1ba62ba8e692260
Florian Beqiraj <florianbeqiraj@MBP.local>
2022-06-07 11:31:34 +0200

    Updated Initialize-Script, added columns/types

--- sql/ammunationdb_initialize.sql
+++ sql/ammunationdb_initialize.sql
@@ -15,47 +15,50 @@ GO
 --        CREATE TABLES
 --===============================================================
 
-CREATE TABLE AmmoType
-(
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_AmmoType             PRIMARY KEY
-   ,Designation             NVARCHAR(50)        NOT NULL
-)
-
 CREATE TABLE WeaponCategory
 (
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_WeaponCategory       PRIMARY KEY
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_WeaponCategory       PRIMARY KEY
    ,[Name]                  NVARCHAR(50)        NOT NULL
+)
+
+CREATE TABLE AmmoType
+(
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_AmmoType             PRIMARY KEY
+   ,Designation             NVARCHAR(50)        NOT NULL
 )
 
 CREATE TABLE Weapon
 (
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_Weapon               PRIMARY KEY
-   ,AmmoTypeId              UNIQUEIDENTIFIER    NOT NULL
-   ,WeaponCategoryId        UNIQUEIDENTIFIER    NOT NULL
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_Weapon               PRIMARY KEY
+   ,WeaponCategoryId        INT                 NOT NULL
+   ,AmmoTypeId              INT                 NOT NULL
    ,Designation             NVARCHAR(50)        NOT NULL
-   ,WeightInKilogram        INTEGER             NOT NULL
-   ,Origin                  NVARCHAR(50)        NOT NULL
+   ,WeightInKilogram        INTEGER             NULL
+   ,Origin                  NVARCHAR(50)        NULL
+   ,MarketPriceInUsd        MONEY               NULL
+   ,IsLegal                 BIT                 NULL
    ,DateOfManufacture       DATE                NULL
+   ,TimePeriod              NVARCHAR(50)        NULL
 )
 
 CREATE TABLE AttachmentType
 (
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_AttachmentType       PRIMARY KEY
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_AttachmentType       PRIMARY KEY
    ,Designation             NVARCHAR(50)        NOT NULL
 )
 
 CREATE TABLE Attachment
 (
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_Attachment           PRIMARY KEY
-   ,AttachmentTypeId        UNIQUEIDENTIFIER    NOT NULL
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_Attachment           PRIMARY KEY
+   ,AttachmentTypeId        INT                 NOT NULL
    ,Designation             NVARCHAR(50)        NOT NULL
 )
 
 CREATE TABLE WeaponPerAttachment
 (
-    Id                      UNIQUEIDENTIFIER    NOT NULL    CONSTRAINT  PK_WeaponPerAttachment  PRIMARY KEY
-   ,WeaponId                UNIQUEIDENTIFIER    NOT NULL
-   ,AttachmentId            UNIQUEIDENTIFIER    NOT NULL
+    Id                      INT IDENTITY(1, 1)  NOT NULL    CONSTRAINT  PK_WeaponPerAttachment  PRIMARY KEY
+   ,WeaponId                INT                 NOT NULL
+   ,AttachmentId            INT                 NOT NULL
 )
 
 GO
